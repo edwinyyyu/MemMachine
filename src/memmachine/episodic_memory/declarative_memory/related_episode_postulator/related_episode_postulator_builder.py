ede0015191e57684b2ba954ccc22bb62822a6b5f
Edwin Yu <edwinyyyu@gmail.com>
2025-10-10 13:34:46 -0700

    Update RelatedEpisodePostulatorBuilder
    Signed-off-by: Edwin Yu <edwinyyyu@gmail.com>

--- src/memmachine/episodic_memory/declarative_memory/related_episode_postulator/related_episode_postulator_builder.py
+++ src/memmachine/episodic_memory/declarative_memory/related_episode_postulator/related_episode_postulator_builder.py
@@ -43,12 +43,10 @@ class RelatedEpisodePostulatorBuilder(Builder):
                 )
 
                 populated_config = {
-                    "vector_graph_store": injections[config["vector_graph_store_id"]],
-                    "search_limit": config.get("search_limit", 1),
-                    "filterable_property_keys": config.get(
-                        "filterable_property_keys", set()
-                    ),
-                }
+                    key: value
+                    for key, value in config.items()
+                    if key != "vector_graph_store_id"
+                } | {"vector_graph_store": injections[config["vector_graph_store_id"]]}
                 return PreviousRelatedEpisodePostulator(populated_config)
             case _:
                 raise ValueError(f"Unknown RelatedEpisodePostulator variant: {variant}")
