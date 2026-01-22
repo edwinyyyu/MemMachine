f3ce54794e75f5ef5a10a60b732e57ca614a1b93
Edwin Yu <edwinyyyu@gmail.com>
2026-01-22 13:31:19 -0800

    Create vector store interface
    Signed-off-by: Edwin Yu <edwinyyyu@gmail.com>

---
+++ src/memmachine/common/vector_store/__init__.py
@@ -0,0 +1,10 @@
+"""Public exports for vector store."""
+
+from .data_types import PropertyValue, Record
+from .vector_store import VectorStore
+
+__all__ = [
+    "PropertyValue",
+    "Record",
+    "VectorStore",
+]
