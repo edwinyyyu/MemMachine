2ec6b4a5fa8b67994a1646a3f793b644ad14ba20
Edwin Yu <edwinyyyu@gmail.com>
2026-02-10 14:19:02 -0800

    WIP: Normalize similarities to [0, 1]
    Signed-off-by: Edwin Yu <edwinyyyu@gmail.com>

--- src/memmachine/common/data_types.py
+++ src/memmachine/common/data_types.py
@@ -7,11 +7,38 @@ FilterablePropertyValue = bool | int | float | str | datetime | None
 
 
 class SimilarityMetric(Enum):
-    """Similarity metrics supported by embedding operations."""
+    """Similarity metrics supported by embedding operations. All scores are normalized to [0, 1], where higher is more similar."""
 
+    """
+    COSINE
+    with cosine_similarity as the raw cosine similarity in [-1, 1]:
+    score formula: S = (1 + cosine_similarity) / 2, where cosine_similarity is the raw cosine in [-1, 1].
+    inverse formula: cosine_similarity = 2 * S - 1
+    """
     COSINE = "cosine"
+
+    """
+    DOT
+    with dot_product as the raw dot product in (-inf, inf):
+    score formula: S = (1 + dot_product / sqrt(1 + dot_product^2)) / 2
+    inverse formula: dot_product = (2 * S - 1) / sqrt(1 - (2 * S - 1)^2)
+    """
     DOT = "dot"
+
+    """
+    EUCLIDEAN
+    with euclidean_distance as the raw Euclidean distance in [0, inf):
+    score formula: S = 1 / (1 + euclidean_distance)
+    inverse formula: euclidean_distance = 1 / S - 1
+    """
     EUCLIDEAN = "euclidean"
+
+    """
+    MANHATTAN
+    with manhattan_distance as the raw Manhattan distance in [0, inf):
+    score formula: S = 1 / (1 + manhattan_distance)
+    inverse formula: manhattan_distance = 1 / S - 1
+    """
     MANHATTAN = "manhattan"
 
 
