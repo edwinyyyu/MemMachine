e7b3b26bcad3e4912b9d7bab8b9bff14a19c3c8f
Edwin Yu <edwinyyyu@gmail.com>
2026-03-05 13:25:16 -0800

    Implement QdrantVectorStore
    Signed-off-by: Edwin Yu <edwinyyyu@gmail.com>

--- packages/server/src/memmachine_server/common/vector_store/__init__.py
+++ packages/server/src/memmachine_server/common/vector_store/__init__.py
@@ -1,10 +1,11 @@
 """Public exports for vector store."""
 
-from .data_types import QueryResult, Record
+from .data_types import QueryMatch, QueryResult, Record
 from .vector_store import Collection, VectorStore
 
 __all__ = [
     "Collection",
+    "QueryMatch",
     "QueryResult",
     "Record",
     "VectorStore",
