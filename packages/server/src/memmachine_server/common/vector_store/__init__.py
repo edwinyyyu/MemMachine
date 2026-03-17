a12e15cfc9c4d90192ea5e734b31032f1326dad6
Edwin Yu <edwinyyyu@gmail.com>
2026-03-16 17:18:21 -0700

    Revert "Revert two commits"
    This reverts commit 08ecc9671d4a6994eb6f64d01fe68226730e705e.

--- packages/server/src/memmachine_server/common/vector_store/__init__.py
+++ packages/server/src/memmachine_server/common/vector_store/__init__.py
@@ -1,10 +1,17 @@
 """Public exports for vector store."""
 
 from .data_types import QueryResult, Record
-from .vector_store import Collection, VectorStore
+from .vector_store import (
+    Collection,
+    CollectionAlreadyExistsError,
+    CollectionNotFoundError,
+    VectorStore,
+)
 
 __all__ = [
     "Collection",
+    "CollectionAlreadyExistsError",
+    "CollectionNotFoundError",
     "QueryResult",
     "Record",
     "VectorStore",
