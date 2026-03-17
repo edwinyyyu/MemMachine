a4b16150d477622a39f964b7af1b321278946c6f
Edwin Yu <edwinyyyu@gmail.com>
2026-03-17 16:32:04 -0700

    Add new error
    Signed-off-by: Edwin Yu <edwinyyyu@gmail.com>

--- packages/server/src/memmachine_server/common/vector_store/__init__.py
+++ packages/server/src/memmachine_server/common/vector_store/__init__.py
@@ -4,12 +4,14 @@ from .data_types import QueryResult, Record
 from .vector_store import (
     Collection,
     CollectionAlreadyExistsError,
+    CollectionConfigurationMismatchError,
     VectorStore,
 )
 
 __all__ = [
     "Collection",
     "CollectionAlreadyExistsError",
+    "CollectionConfigurationMismatchError",
     "QueryResult",
     "Record",
     "VectorStore",
