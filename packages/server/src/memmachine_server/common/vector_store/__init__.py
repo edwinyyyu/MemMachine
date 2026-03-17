efdbd513cb419a2c65518ed27a872a4c0e465a90
Edwin Yu <edwinyyyu@gmail.com>
2026-03-17 16:14:01 -0700

    Remove unnecessary error
    Signed-off-by: Edwin Yu <edwinyyyu@gmail.com>

--- packages/server/src/memmachine_server/common/vector_store/__init__.py
+++ packages/server/src/memmachine_server/common/vector_store/__init__.py
@@ -4,14 +4,12 @@ from .data_types import QueryResult, Record
 from .vector_store import (
     Collection,
     CollectionAlreadyExistsError,
-    CollectionNotFoundError,
     VectorStore,
 )
 
 __all__ = [
     "Collection",
     "CollectionAlreadyExistsError",
-    "CollectionNotFoundError",
     "QueryResult",
     "Record",
     "VectorStore",
