6d5ba99d8943b2bca076252635dc6662b230e58b
Edwin Yu <edwinyyyu@gmail.com>
2026-03-25 14:21:05 -0700

    Simplify APIs and use exact deduplication
    Signed-off-by: Edwin Yu <edwinyyyu@gmail.com>

--- packages/server/src/memmachine_server/episodic_memory/extra_memory/segment_linker/__init__.py
+++ packages/server/src/memmachine_server/episodic_memory/extra_memory/segment_linker/__init__.py
@@ -1,13 +1,11 @@
 """Segment store data models and interfaces."""
 
 from .segment_linker import (
-    DerivativeNotActiveError,
     SegmentLinker,
     SegmentLinkerPartition,
 )
 
 __all__ = [
-    "DerivativeNotActiveError",
     "SegmentLinker",
     "SegmentLinkerPartition",
 ]
